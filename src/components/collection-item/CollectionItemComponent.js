b95997eed646728822149254147278382b3ed0ea
Dimitris <divergos@gmail.com>
2020-04-13 16:10:27 +0300

    updated shop data with new data, updated cart utils to group item in cart reducer

--- src/components/collection-item/CollectionItemComponent.js
+++ src/components/collection-item/CollectionItemComponent.js
@@ -1,9 +1,17 @@
 import React from "react";
 
+import { connect } from 'react-redux';
+import { addItem } from '../../redux/cart/cart.actions';
+
+import CustomButton from '../custom-button/CustomButtonComponent';
+
 import "./collection-item.styles.scss";
 
-const CollectionItem = ({ id, name, price, imageUrl }) => (
-    <div className="collection-item">
+const CollectionItem = ({ item, addItem }) => {
+
+    const { id, name, price, imageUrl } = item;
+
+    return <div className="collection-item">
         <div
             className="image"
             style={{
@@ -15,7 +23,12 @@ const CollectionItem = ({ id, name, price, imageUrl }) => (
             <span className="name">{name}</span>
             <span className="price">{price}</span>
         </div>
+        <CustomButton onClick={() => addItem(item)} inverted>Add to cart</CustomButton>
     </div>
-);
+}
 
-export default CollectionItem;
+const mapDispatchToProps = dispatch => (
+    { addItem: (item) => dispatch(addItem(item)) }
+)
+
+export default connect(null, mapDispatchToProps)(CollectionItem);
--- src/components/preview-collection/PreviewCollectionComponent.js
+++ src/components/preview-collection/PreviewCollectionComponent.js
@@ -10,8 +10,8 @@ const PreviewCollection = ({ title, items }) => (
         <h1 className="title">{title.toUpperCase()}</h1>
         <div className='preview'>
             {
-                items.filter((item, index) => index < 4).map(({ id, ...itemsProps }) => (
-                    <CollectionItem key={id} {...itemsProps} />
+                items.filter((item, index) => index < 4).map((item) => (
+                    <CollectionItem key={item.id} item={item} />
                 ))
             }
         </div>
